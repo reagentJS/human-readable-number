7336745cca1234d213a971c145974aa25f1e8789
reagentjs <reagentjs@gmail.com>
2020-09-26 22:09:02 +0300

    feat: add implementation of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+    const one = {
+        0: 'zero',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+    };
+
+    const two = {
+        20: 'twenty',
+        30: 'thirty',
+        40: 'forty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety',
+    };
+
+    if (number < 20) return one[number];
+    let numberToArray = number.toString().split('');
+    let res = '';
+
+    function twoDigits(numberToArray) {
+        let res = `${two[numberToArray[0].concat('0')]} ${one[numberToArray[1]]}`;
+        if (numberToArray[1] === '0') res = res.slice(0, -5);
+        return res;
+    }
+
+    if (numberToArray.length === 2) return twoDigits(numberToArray);
+
+    if (numberToArray.length === 3) {
+        res = `${one[numberToArray[0]]} hundred`;
+        let sliced = numberToArray.slice(1).join('');
+        if (sliced === '00') return res;
+        if (sliced < 20) res = `${res} ${one[+sliced]}`;
+        else res = `${res} ${twoDigits(sliced)}`;
+    }
+
+    return res;
 }
